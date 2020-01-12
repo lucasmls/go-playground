6001074677f5b298bcfadc66bfb94b1065970cba
Lucas Mendes <lucaslsbh1@gmail.com>
2020-01-12 20:34:33 -0300

    showcase of fuctions with named return

--- fundamentals/functions/functions.go
+++ fundamentals/functions/functions.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func sum(n1 int, n2 int) int {
 	return n1 + n2
+}
+
+// Function with named return
+// we don't need to add the variables in return because
+// it was already specified in the function declaration
+func getNameAndSurname(name string) (firstName, surname string) {
+	splittedName := strings.Split(name, " ")
+	firstName = splittedName[0]
+	surname = splittedName[1]
+	return
 }
 
 func printResult(result int) {
@@ -13,4 +26,7 @@ func printResult(result int) {
 func main() {
 	result := sum(10, 10)
 	printResult(result)
+
+	name, surname := getNameAndSurname("Lucas Mendes")
+	fmt.Println(name, surname)
 }
