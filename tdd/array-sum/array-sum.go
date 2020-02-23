f14c5554c4f0aa6acd16279c8d18317f9f79473f
Lucas Mendes <lucaslsbh1@gmail.com>
2020-02-23 19:44:50 -0300

    tdd(feat): added sumAllTails func and its tests

--- tdd/array-sum/array-sum.go
+++ tdd/array-sum/array-sum.go
@@ -20,3 +20,19 @@ func SumAll(listsToSum ...[]int) []int {
 
 	return sums
 }
+
+// SumAllTails ...
+func SumAllTails(listsToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range listsToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
--- tdd/array-sum/array-sum_test.go
+++ tdd/array-sum/array-sum_test.go
@@ -39,3 +39,23 @@ func TestSumAll(t *testing.T) {
 		}
 	})
 }
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("expect to sum only the tail of the slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got '%v' want '%v", got, want)
+		}
+	})
+
+	t.Run("expect to safely sum the slices tail", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got '%v' want '%v", got, want)
+		}
+	})
+}
