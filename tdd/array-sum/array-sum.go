11841a7d8ae2d2899c61a16c881ff548ae5ed855
Lucas Mendes <lucaslsbh1@gmail.com>
2020-02-23 00:32:44 -0300

    tdd(refac): using range to iterate over the numbers

--- tdd/array-sum/array-sum.go
+++ tdd/array-sum/array-sum.go
@@ -3,8 +3,8 @@ package sum
 // Sum ...
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum
