fff7efc66a69c2514eab9d04aafd412c9ad31b66
Lucas Mendes <lucaslsbh1@gmail.com>
2020-02-23 00:37:09 -0300

    tdd(refac): array sum func to accept a slice of numbers, instead of a [5]int array

--- tdd/array-sum/array-sum.go
+++ tdd/array-sum/array-sum.go
@@ -1,7 +1,7 @@
 package sum
 
 // Sum ...
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
--- tdd/array-sum/array-sum_test.go
+++ tdd/array-sum/array-sum_test.go
@@ -3,11 +3,22 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("should sum all of the array items", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("should sum all of the five array items", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-		got := Sum((numbers))
+		got := Sum(numbers)
 		want := 15
+
+		if got != want {
+			t.Errorf("got '%d' want '%d' given '%v'", got, want, numbers)
+		}
+	})
+
+	t.Run("should sum all of the ten array items", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		got := Sum(numbers)
+		want := 55
 
 		if got != want {
 			t.Errorf("got '%d' want '%d' given '%v'", got, want, numbers)
