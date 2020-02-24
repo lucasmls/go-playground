61e247a4e1597ba8ebea6d9ab4898d362d578514
Lucas Mendes <lucaslsbh1@gmail.com>
2020-02-24 19:23:43 -0300

    tdd(feat) dealing with not found words in dictionary

--- tdd/dictionary/dictionary.go
+++ tdd/dictionary/dictionary.go
@@ -1,9 +1,16 @@
 package dictionary
 
+import "errors"
+
 // Dictionary ...
 type Dictionary map[string]string
 
 // Search ...
-func (d Dictionary) Search(dictionary map[string]string, word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
--- tdd/dictionary/dictionary_test.go
+++ tdd/dictionary/dictionary_test.go
@@ -3,14 +3,25 @@ package dictionary
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{
-		"name": "Laisla Pinto Coelho",
-	}
+	dictionary := Dictionary{"name": "Laisla Pinto Coelho"}
 
-	got := dictionary.Search(dictionary, "name")
-	want := "Laisla Pinto Coelho"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("name")
+		want := "Laisla Pinto Coelho"
 
-	assertString(t, got, want)
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertString(t, err.Error(), want)
+	})
 }
 
 func assertString(t *testing.T, got, want string) {
