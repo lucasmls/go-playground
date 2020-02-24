e1f3fa4f191a16e755aa94fcddc36f7b7bda206c
Lucas Mendes <lucaslsbh1@gmail.com>
2020-02-24 17:40:05 -0300

    tdd(feat): made wallet a wallet of btc

--- tdd/wallet/wallet.go
+++ tdd/wallet/wallet.go
@@ -1,16 +1,30 @@
 package wallet
 
+import "fmt"
+
+// Stringer ...
+type Stringer interface {
+	String() string
+}
+
+// Bitcoin ...
+type Bitcoin int
+
 // Wallet ...
 type Wallet struct {
-	balance int
+	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
 // Deposit ...
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Balance ...
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- tdd/wallet/wallet_test.go
+++ tdd/wallet/wallet_test.go
@@ -1,15 +1,17 @@
 package wallet
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
-		t.Errorf("got '%d' want '%d'", got, want)
+		t.Errorf("got '%s' want '%s'", got, want)
 	}
 }
