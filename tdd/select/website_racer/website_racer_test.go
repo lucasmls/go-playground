b648298a99684a25b8c563363ad4af803039006e
Lucas Mendes <lucaslsbh1@gmail.com>
2020-04-01 07:28:20 -0300

    tdd(refac): added defer to close connection with httptest server

--- tdd/select/website_racer/website_racer_test.go
+++ tdd/select/website_racer/website_racer_test.go
@@ -11,6 +11,9 @@ func TestRace(t *testing.T) {
 	slowServer := makeDelayedServer(20 * time.Millisecond)
 	fastServer := makeDelayedServer(0 * time.Millisecond)
 
+	defer slowServer.Close()
+	defer fastServer.Close()
+
 	slowURL := slowServer.URL
 	fastURL := fastServer.URL
 
@@ -20,9 +23,6 @@ func TestRace(t *testing.T) {
 	if got != want {
 		t.Errorf("got: %q wanted: %q", got, want)
 	}
-
-	slowServer.Close()
-	fastServer.Close()
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
