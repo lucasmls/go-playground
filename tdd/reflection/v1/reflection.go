83d4b71ff3c67976e44547b23ddbb557a033bee1
Lucas Mendes <lucaslsbh1@gmail.com>
2020-04-07 08:11:46 -0300

    tdd(refac): only DRY the recursive walk call, letting the value extraction to the switch cases

--- tdd/reflection/v1/reflection.go
+++ tdd/reflection/v1/reflection.go
@@ -5,29 +5,29 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	numberOfValues := 0
-	var getField func(int) reflect.Value
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
 
 	switch val.Kind() {
 	case reflect.String:
 		fn(val.String())
 
 	case reflect.Struct:
-		numberOfValues = val.NumField()
-		getField = val.Field
-
-	case reflect.Map:
-		for _, key := range val.MapKeys() {
-			walk(val.MapIndex(key).Interface(), fn)
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
 		}
 
 	case reflect.Slice, reflect.Array:
-		numberOfValues = val.Len()
-		getField = val.Index
-	}
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
 
-	for i := 0; i < numberOfValues; i++ {
-		walk(getField(i).Interface(), fn)
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+
 	}
 }
 
