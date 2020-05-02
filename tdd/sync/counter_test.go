c67c027843ecc795c77f2bee2721ee3ba7dd25e5
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-02 00:14:33 -0300

    tdd(chore): refac assert counter value to a func

--- tdd/sync/counter_test.go
+++ tdd/sync/counter_test.go
@@ -2,15 +2,21 @@ package main
 
 import "testing"
 
+func assertCounterValue(t *testing.T, got Counter, want int) {
+	t.Helper()
+
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
 func TestCounter(t *testing.T) {
+
 	t.Run("incrementing the counting three times leaves it at 3", func(t *testing.T) {
 		counter := Counter{}
 		counter.Increment()
 		counter.Increment()
 		counter.Increment()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
-		}
+		assertCounterValue(t, counter, 3)
 	})
 }
