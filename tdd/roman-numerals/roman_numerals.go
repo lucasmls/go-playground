5b83418e83842f0955476440c672040eaad3180c
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-03 16:00:53 -0300

    tdd(refac): to keep the strategy of string.builder

--- tdd/roman-numerals/roman_numerals.go
+++ tdd/roman-numerals/roman_numerals.go
@@ -4,13 +4,14 @@ import "strings"
 
 // ConvertToRoman converts a arabic number to a roman number
 func ConvertToRoman(num int) string {
-	if num == 4 {
-		return "IV"
-	}
-
 	var result strings.Builder
 
-	for i := 0; i < num; i++ {
+	for i := num; i > 0; i-- {
+		if num == 4 {
+			result.WriteString("IV")
+			break
+		}
+
 		result.WriteString("I")
 	}
 
