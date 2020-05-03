17ec9f3df7eec551bc7e5a0ae16380f3a6822b7d
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-03 15:01:42 -0300

    tdd(refac): roman converter to table test structure

--- tdd/roman-numerals/roman-numerals_test.go
+++ tdd/roman-numerals/roman-numerals_test.go
@@ -3,21 +3,32 @@ package numerals
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	t.Run(`"1" gets converted to "I"`, func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
 
-		if got != want {
-			t.Errorf(`got "%q" want "%q"`, got, want)
-		}
-	})
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{
+			Description: `"1" gets converted to "I"`,
+			Arabic:      1,
+			Want:        "I",
+		},
+		{
+			Description: `"2" gets converted to "II"`,
+			Arabic:      2,
+			Want:        "II",
+		},
+	}
 
-	t.Run(`"2" gets converted to "II"`, func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			want := test.Want
 
-		if got != want {
-			t.Errorf(`got "%q", want "%q"`, got, want)
-		}
-	})
+			if got != want {
+				t.Errorf(`got "%q" want "%q"`, got, want)
+			}
+		})
+	}
 }
