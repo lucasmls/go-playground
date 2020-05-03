16a34897ede62afebd11a49d5248e4494dbae006
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-03 19:06:56 -0300

    tdd(refac): tests to short syntax

--- tdd/roman-numerals/roman-numerals_test.go
+++ tdd/roman-numerals/roman-numerals_test.go
@@ -9,126 +9,30 @@ func TestRomanNumerals(t *testing.T) {
 		Arabic      int
 		Want        string
 	}{
-		{
-			Description: `1 gets converted to I`,
-			Arabic:      1,
-			Want:        "I",
-		},
-		{
-			Description: `2 gets converted to II`,
-			Arabic:      2,
-			Want:        "II",
-		},
-		{
-			Description: `3 gets converted to III`,
-			Arabic:      3,
-			Want:        "III",
-		},
-		{
-			Description: `4 gets converted to IV (can't repeat more than 3 times)`,
-			Arabic:      4,
-			Want:        "IV",
-		},
-		{
-			Description: `5 gets converted to V`,
-			Arabic:      5,
-			Want:        "V",
-		},
-		{
-			Description: `6 gets converted to VI`,
-			Arabic:      6,
-			Want:        "VI",
-		},
-		{
-			Description: `7 gets converted to VII`,
-			Arabic:      7,
-			Want:        "VII",
-		},
-		{
-			Description: `8 gets converted to VIII`,
-			Arabic:      8,
-			Want:        "VIII",
-		},
-		{
-			Description: `9 gets converted to IX`,
-			Arabic:      9,
-			Want:        "IX",
-		},
-		{
-			Description: `10 gets converted to X`,
-			Arabic:      10,
-			Want:        "X",
-		},
-		{
-			Description: `11 gets converted to XI`,
-			Arabic:      11,
-			Want:        "XI",
-		},
-		{
-			Description: `12 gets converted to XII`,
-			Arabic:      12,
-			Want:        "XII",
-		},
-		{
-			Description: `13 gets converted to XIII`,
-			Arabic:      13,
-			Want:        "XIII",
-		},
-		{
-			Description: `14 gets converted to XIV`,
-			Arabic:      14,
-			Want:        "XIV",
-		},
-		{
-			Description: "18 gets converted to XVIII",
-			Arabic:      18,
-			Want:        "XVIII",
-		},
-		{
-			Description: "20 gets converted to XX",
-			Arabic:      20,
-			Want:        "XX",
-		},
-		{
-			Description: "39 gets converted to XXXIX",
-			Arabic:      39,
-			Want:        "XXXIX",
-		},
-		{
-			Description: "50 gets converted to L",
-			Arabic:      50,
-			Want:        "L",
-		},
-		{
-			Description: "100 gets converted to C",
-			Arabic:      100,
-			Want:        "C",
-		},
-		{
-			Description: "500 gets converted to D",
-			Arabic:      500,
-			Want:        "D",
-		},
-		{
-			Description: "1000 gets converted to M",
-			Arabic:      1000,
-			Want:        "M",
-		},
-		{
-			Description: "1984 gets converted to MCMLXXXIV",
-			Arabic:      1984,
-			Want:        "MCMLXXXIV",
-		},
-		{
-			Description: "798 gets converted to DCCXCVIII",
-			Arabic:      798,
-			Want:        "DCCXCVIII",
-		},
-		{
-			Description: "1006 gets converted to MVI",
-			Arabic:      1006,
-			Want:        "MVI",
-		},
+		{`1 gets converted to I`, 1, "I"},
+		{`2 gets converted to II`, 2, "II"},
+		{`3 gets converted to III`, 3, "III"},
+		{`4 gets converted to IV (can't repeat more than 3 times)`, 4, "IV"},
+		{`5 gets converted to V`, 5, "V"},
+		{`6 gets converted to VI`, 6, "VI"},
+		{`7 gets converted to VII`, 7, "VII"},
+		{`8 gets converted to VIII`, 8, "VIII"},
+		{`9 gets converted to IX`, 9, "IX"},
+		{`10 gets converted to X`, 10, "X"},
+		{`11 gets converted to XI`, 11, "XI"},
+		{`12 gets converted to XII`, 12, "XII"},
+		{`13 gets converted to XIII`, 13, "XIII"},
+		{`14 gets converted to XIV`, 14, "XIV"},
+		{"18 gets converted to XVIII", 18, "XVIII"},
+		{"20 gets converted to XX", 20, "XX"},
+		{"39 gets converted to XXXIX", 39, "XXXIX"},
+		{"50 gets converted to L", 50, "L"},
+		{"100 gets converted to C", 100, "C"},
+		{"500 gets converted to D", 500, "D"},
+		{"1000 gets converted to M", 1000, "M"},
+		{"1984 gets converted to MCMLXXXIV", 1984, "MCMLXXXIV"},
+		{"798 gets converted to DCCXCVIII", 798, "DCCXCVIII"},
+		{"1006 gets converted to MVI", 1006, "MVI"},
 	}
 
 	for _, test := range cases {
