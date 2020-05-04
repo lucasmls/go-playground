7f54fd2edcbe8105692d720279d5af85ff964293
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-04 07:17:10 -0300

    tdd(wip): added second to radians func

--- tdd/clock/clock.go
+++ tdd/clock/clock.go
@@ -1,11 +1,19 @@
 package clock
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // Point represents a two dimensional Cartesian coordinate
 type Point struct {
 	X float64
 	Y float64
+}
+
+func secondsInRadians(t time.Time) float64 {
+	// return float64(t.Second()) * (math.Pi / 30)
+	return (math.Pi / (30 / (float64(t.Second()))))
 }
 
 // SecondHand is the unit vector of the second hand of a analogue clock at time 't'
--- tdd/clock/clock_test.go
+++ tdd/clock/clock_test.go
@@ -1,19 +1,61 @@
-package clock_test
+package clock
 
 import (
+	"math"
 	"testing"
 	"time"
-
-	"github.com/lucasmls/tdd/clock"
 )
 
-func Test_SecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+func Test_SecondHand(t *testing.T) {
+	// t.Run("second hand at midnight", func(t *testing.T) {
+	// 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clock.Point{X: 150, Y: 150 - 90}
-	got := clock.SecondHand(tm)
+	// 	want := clock.Point{X: 150, Y: 150 - 90}
+	// 	got := clock.SecondHand(tm)
 
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
+	// 	if got != want {
+	// 		t.Errorf("Got %v, wanted %v", got, want)
+	// 	}
+	// })
+
+	// t.Run("second hand at 30 seconds", func(t *testing.T) {
+	// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	// 	want := clock.Point{X: 150, Y: 150 + 90}
+	// 	got := clock.SecondHand(tm)
+
+	// 	if got != want {
+	// 		t.Errorf("Got %v, wanted %v", got, want)
+	// 	}
+	// })
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(321, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func Test_SecondsInRadians(t *testing.T) {
+
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{time: simpleTime(0, 0, 0), angle: 0},
+		{time: simpleTime(0, 0, 30), angle: math.Pi},
+		{time: simpleTime(0, 0, 45), angle: (math.Pi / 2) * 3},
+		{time: simpleTime(0, 0, 7), angle: (math.Pi / 30) * 7},
+	}
+
+	for _, c := range cases {
+		want := c.angle
+		got := secondsInRadians(c.time)
+
+		if want != got {
+			t.Fatalf("Wanted %v radians, but got %v", want, got)
+		}
 	}
 }
