1d37c864b082d95f30fa25a0142634ed1d7f1851
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-04 07:32:14 -0300

    tdd(wip): added secondHandPoint func

--- tdd/clock/clock.go
+++ tdd/clock/clock.go
@@ -16,6 +16,15 @@ func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
 
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
+
 // SecondHand is the unit vector of the second hand of a analogue clock at time 't'
 // represented as a Point.
 func SecondHand(t time.Time) Point {
--- tdd/clock/clock_test.go
+++ tdd/clock/clock_test.go
@@ -6,36 +6,43 @@ import (
 	"time"
 )
 
-func Test_SecondHand(t *testing.T) {
-	// t.Run("second hand at midnight", func(t *testing.T) {
-	// 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	// 	want := clock.Point{X: 150, Y: 150 - 90}
-	// 	got := clock.SecondHand(tm)
-
-	// 	if got != want {
-	// 		t.Errorf("Got %v, wanted %v", got, want)
-	// 	}
-	// })
-
-	// t.Run("second hand at 30 seconds", func(t *testing.T) {
-	// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-	// 	want := clock.Point{X: 150, Y: 150 + 90}
-	// 	got := clock.SecondHand(tm)
-
-	// 	if got != want {
-	// 		t.Errorf("Got %v, wanted %v", got, want)
-	// 	}
-	// })
-}
-
 func testName(t time.Time) string {
 	return t.Format("15:04:05")
 }
 
 func simpleTime(hours, minutes, seconds int) time.Time {
 	return time.Date(321, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func roughlyEqualFloat64(a, b float64) bool {
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+}
+
+func roughlyEqualPoint(a, b Point) bool {
+	return roughlyEqualFloat64(a.X, b.X) && roughlyEqualFloat64(a.Y, b.Y)
+}
+
+func Test_SecondHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{time: simpleTime(0, 0, 30), point: Point{0, -1}},
+		{time: simpleTime(0, 0, 45), point: Point{-1, 0}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondHandPoint(c.time)
+			want := c.point
+
+			if !roughlyEqualPoint(got, want) {
+				t.Fatalf("Wanted %v Point, but got %v", want, got)
+			}
+		})
+	}
+
 }
 
 func Test_SecondsInRadians(t *testing.T) {
