8d7637d0e6583202bccbc8102750bc9c4bc62652
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-04 07:42:58 -0300

    tdd(refac): extract some constants to use  in SecondHand func

--- tdd/clock/clock.go
+++ tdd/clock/clock.go
@@ -25,13 +25,20 @@ func secondHandPoint(t time.Time) Point {
 	return Point{x, y}
 }
 
+const (
+	secondHandLength = 90
+	clockCentreX     = 150
+	clockCentreY     = 150
+)
+
 // SecondHand is the unit vector of the second hand of a analogue clock at time 't'
 // represented as a Point.
 func SecondHand(t time.Time) Point {
 	p := secondHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90}   // scale
-	p = Point{p.X, -p.Y}            // flip
-	p = Point{p.X + 150, p.Y + 150} // translate
+
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      // flip
+	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // translate
 
 	return p
 }
