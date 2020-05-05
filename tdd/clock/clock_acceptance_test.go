ea2b697e5ddb36a22b151889d45c0f77b79ad455
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-05 09:31:40 -0300

    tdd(feat): end clock drawing the hour hand

--- tdd/clock/clock_acceptance_test.go
+++ tdd/clock/clock_acceptance_test.go
@@ -86,6 +86,29 @@ func Test_SVGWriterMinuteHand(t *testing.T) {
 	}
 }
 
+func Test_SVGWriterHourHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{simpleTime(6, 0, 0), Line{150, 150, 150, 200}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			clock.SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the hour hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
 func containsLine(l Line, ls []Line) bool {
 	for _, line := range ls {
 		if line == l {
