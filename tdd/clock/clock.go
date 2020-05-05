1573be35c23eded0a236de1604535dcfef662c2c
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-05 09:24:00 -0300

    tdd(wip): added hours in radians func

--- tdd/clock/clock.go
+++ tdd/clock/clock.go
@@ -20,6 +20,10 @@ func minutesInRadians(t time.Time) float64 {
 	return (secondsInRadians(t) / 60) + (math.Pi / (30 / float64(t.Minute())))
 }
 
+func hoursInRadians(t time.Time) float64 {
+	return (minutesInRadians(t) / 12) + (math.Pi / (6 / float64(t.Hour()%12)))
+}
+
 func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
--- tdd/clock/clock_test.go
+++ tdd/clock/clock_test.go
@@ -107,3 +107,26 @@ func Test_MinutesInRadians(t *testing.T) {
 		})
 	}
 }
+
+func TestHoursInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(6, 0, 0), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(21, 0, 0), math.Pi * 1.5},
+		{simpleTime(0, 1, 30), math.Pi / ((6 * 60 * 60) / 90)},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := hoursInRadians(c.time)
+			want := c.angle
+
+			if !roughlyEqualFloat64(got, want) {
+				t.Fatalf("Wanted %v radians, but got %v", want, got)
+			}
+		})
+	}
+}
